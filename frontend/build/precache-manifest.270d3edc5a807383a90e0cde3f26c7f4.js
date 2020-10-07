fce422eefc181d427a8d7feb7140786af4f3133b
DMouhy <50054600+DMouhy@users.noreply.github.com>
2020-10-07 03:03:18 +0100

    Add files via upload

---
+++ frontend/build/precache-manifest.270d3edc5a807383a90e0cde3f26c7f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399d94d205a31a090334f1e90e0bd75a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5acddd95765af614b4ca",
+    "url": "/static/css/main.e61b5b48.chunk.css"
+  },
+  {
+    "revision": "1feb3aaa1c8622a05d04",
+    "url": "/static/js/2.0fea3590.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0fea3590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5acddd95765af614b4ca",
+    "url": "/static/js/main.8b2881e5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "0cf9ebdb0b48b43b92dd31c5cf7851a3",
+    "url": "/static/media/banana_img.0cf9ebdb.jpg"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98e6b66f866ddf8e328e1d413b2153b5.js"
+  "/precache-manifest.270d3edc5a807383a90e0cde3f26c7f4.js"
 );
 
 self.addEventListener('message', (event) => {
